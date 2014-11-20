2f65ff9d69d5496e20da1172445cf21e0041f60f
tuckervento <jtv10@pitt.edu>
2014-11-20 17:46:51 -0500

    Slight tweak to audio parameters

--- GRAMPS/GRAMPS.ino
+++ GRAMPS/GRAMPS.ino
@@ -30,7 +30,7 @@ void setup()
 
   // 44100 Hz stereo => 88200 sample rate
   // 100 mSec of prebuffering.
-  Audio.begin(44100, 200);
+  Audio.begin(44100, 300);
 }
 
 void loop()
@@ -51,7 +51,7 @@ void loop()
   //i believe unsigned short is faster, but uint16_t is more memory conservative?
   //but i'm not sure
   uint16_t grainTime=100;
-  uint8_t grainRepeat=3;
+  uint8_t grainRepeat=2;
   const uint32_t S=441*(grainTime/10); // Number of samples to read in block
   short buffer[S];
 
@@ -60,7 +60,6 @@ void loop()
   while (myFile.available()) {
     // read from the file into buffer
     myFile.read(buffer, sizeof(buffer));
-
     for (int i = 0; i < grainRepeat; i++) {
       // Prepare samples
       int volume = 1023;
