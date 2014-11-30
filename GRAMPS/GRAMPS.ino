eed7238fe4adab148d3d655c2dbb745594011e77
tuckervento <jtv10@pitt.edu>
2014-11-30 14:54:06 -0500

    DE3: Broke calculation and writing into separate parts for memory conservation, as the attack calculation loop was using up too much memory when done at once.  Included is a fix for DE2, the number of samples was not calculated correctly.

--- GRAMPS/GRAMPS.ino
+++ GRAMPS/GRAMPS.ino
@@ -11,7 +11,7 @@ void setup()
 
   // setup SD-card
   Serial.print("Initializing SD card...");
-  if (!SD.begin(8)) {
+  if (!SD.begin(8, 4)) {
     Serial.println(" failed!");
     return;
   }
@@ -37,36 +37,60 @@ void loop()
   //not sure of unsigned short vs uint16_t, etc...
   //i believe unsigned short is faster, but uint16_t is more memory conservative?
   //but i'm not sure
-  uint16_t grainTime=100;
-  uint8_t grainRepeat=4;
-  const uint32_t S=441*(grainTime/10); // Number of samples to read in block
-  int16_t buf[S];
+  uint16_t grainTime=200;
+  uint8_t grainRepeat=1;
+  const uint16_t S = 441*(grainTime/10); // Number of samples to read in block
+  const uint16_t B = 1024; //fix at 1024 for now
+  int16_t buf[B];
   uint16_t volume = 1023;
 
-  uint8_t attackSetting = 40, attackCounter = 0;
-  uint8_t releaseSetting = 0, releaseCounter = 0;
+  uint8_t attackSetting = 30, releaseSetting = 30;
 
-  uint32_t attackSamples = S / attackSetting, releaseSamples = S / releaseSetting;
+  uint16_t attackSamples = S * (attackSetting / 100.0), releaseSamples = S * (releaseSetting / 100.0);
+  uint16_t relativeEnvelopeCounter, attackCounter = 0, releaseCounter = 0;
+  
+  uint16_t samplesRemaining = S, samplesToRead = B;
 
-  Serial.print("Playing");
+  Serial.println(S);
+  Serial.println(attackSamples);
+  Serial.println(releaseSamples);
+  Serial.println("Playing");
   // until the file is not finished
   while (myFile.available()) {
-    // read from the file into buffer
-    myFile.read(buf, sizeof(buf));
-
-    for (attackCounter = 0; attackCounter < attackSamples; attackCounter++) {
-      buf[attackCounter] = (float)buf[attackCounter]*((float)attackCounter/(float)attackSamples);
-    }
-
-    for (releaseCounter = 0; releaseCounter < releaseSamples; releaseCounter++) {
-      buf[S-releaseCounter-1] = (float)buf[S-releaseCounter-1]*((float)releaseCounter/(float)releaseSamples);
-    }
-
-    Audio.prepare(buf, S, volume);
-    for (int i = 0; i < grainRepeat; i++) {
-      // Prepare samples
-      // Feed samples to audio
-      Audio.write(buf, S);
+    //reset counters
+    samplesToRead = B;
+    samplesRemaining = S;
+    attackCounter = 0;
+    releaseCounter = 0;
+    
+    while (samplesRemaining > 0) {
+      //read into buffer
+      myFile.read(buf, samplesToRead*2);
+      
+      if (attackCounter < attackSamples) {
+        for (relativeEnvelopeCounter = 0; relativeEnvelopeCounter < samplesToRead && attackCounter < attackSamples; relativeAttackCounter++) {
+          buf[relativeEnvelopeCounter] *= (attackCounter/(float)attackSamples);
+          attackCounter++;
+        }
+      }
+      
+      if (releaseCounter < attackSamples) {
+        for (relativeEnvelopeCounter = samplesToRead - 1; relativeEnvelopeCounter > 0 && attackCounter < attackSamples; relativeAttackCounter--) {
+          buf[samplesToRead - relativeEnvelopeCounter - 1] *= (attackCounter/(float)attackSamples);
+          releaseCounter++;
+        }
+      }
+  
+      /*for (releaseCounter = 0; releaseCounter < releaseSamples; releaseCounter++) {
+        buf[S-releaseCounter-1] = (float)buf[S-releaseCounter-1]*((float)releaseCounter/(float)releaseSamples);
+      }*/
+  
+      Audio.prepare(buf, samplesToRead, volume);
+      Audio.write(buf, samplesToRead);
+      
+      //adjust samples remaining
+      samplesRemaining -= samplesToRead;
+      if (samplesRemaining < samplesToRead) { samplesToRead = samplesRemaining; }
     }
   }
   myFile.close();
