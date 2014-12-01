fb7b0c32085d7fa4d83e2df41d6c305888898c19
tuckervento <jtv10@pitt.edu>
2014-11-30 23:24:27 -0500

    US22: Working attack and decay (aka release).  Included are renamings to from release- to decay-, and other tweaks.

--- GRAMPS/GRAMPS.ino
+++ GRAMPS/GRAMPS.ino
@@ -25,8 +25,8 @@ void setup()
 void loop()
 {
   // open wave file from sdcard
-  File myFile = SD.open("test.wav");
-  if (!myFile) {
+  File wavFile = SD.open("test.wav");
+  if (!wavFile) {
     // if the file didn't open, print an error and stop
     Serial.println("error opening test.wav");
     while (true);
@@ -37,61 +37,72 @@ void loop()
   //not sure of unsigned short vs uint16_t, etc...
   //i believe unsigned short is faster, but uint16_t is more memory conservative?
   //but i'm not sure
-  uint16_t grainTime=200;
-  uint8_t grainRepeat=1;
+  uint16_t grainTime = 100;
+  uint8_t grainRepeat = 2, grainWriteCounter = 0;
   const uint16_t S = 441*(grainTime/10); // Number of samples to read in block
   const uint16_t B = 1024; //fix at 1024 for now
   int16_t buf[B];
   uint16_t volume = 1023;
 
-  uint8_t attackSetting = 30, releaseSetting = 30;
+  uint8_t attackSetting = 0, decaySetting = 90;
 
-  uint16_t attackSamples = S * (attackSetting / 100.0), releaseSamples = S * (releaseSetting / 100.0);
-  uint16_t relativeEnvelopeCounter, attackCounter = 0, releaseCounter = 0;
+  uint16_t attackSamples = S * (attackSetting / 100.0), decaySamples = S * (decaySetting / 100.0);
+  uint16_t relativeEnvelopeCounter, attackCounter = 0, decayCounter = 0, decayDifference;
   
   uint16_t samplesRemaining = S, samplesToRead = B;
+  unsigned long grainPosition = wavFile.position();
+  uint8_t segmentCounter = 1;
 
-  Serial.println(S);
-  Serial.println(attackSamples);
-  Serial.println(releaseSamples);
   Serial.println("Playing");
   // until the file is not finished
-  while (myFile.available()) {
+  while (wavFile.available()) {
     //reset counters
     samplesToRead = B;
     samplesRemaining = S;
     attackCounter = 0;
-    releaseCounter = 0;
+    decayCounter = decaySamples;
+    segmentCounter = 1;
     
     while (samplesRemaining > 0) {
       //read into buffer
-      myFile.read(buf, samplesToRead*2);
+      wavFile.read(buf, samplesToRead*2);
       
       if (attackCounter < attackSamples) {
-        for (relativeEnvelopeCounter = 0; relativeEnvelopeCounter < samplesToRead && attackCounter < attackSamples; relativeAttackCounter++) {
+        for (relativeEnvelopeCounter = 0; relativeEnvelopeCounter < samplesToRead && attackCounter < attackSamples; relativeEnvelopeCounter++) {
           buf[relativeEnvelopeCounter] *= (attackCounter/(float)attackSamples);
           attackCounter++;
         }
       }
       
-      if (releaseCounter < attackSamples) {
-        for (relativeEnvelopeCounter = samplesToRead - 1; relativeEnvelopeCounter > 0 && attackCounter < attackSamples; relativeAttackCounter--) {
-          buf[samplesToRead - relativeEnvelopeCounter - 1] *= (attackCounter/(float)attackSamples);
-          releaseCounter++;
+      //detect if we're in decay zone
+      //if the decayCounter is a different value, we've already started calculation
+      if ((decayCounter != decaySamples) || (S - decaySamples) < (B * segmentCounter)) {
+        decayDifference = (B * segmentCounter - (S - decaySamples));
+        for (relativeEnvelopeCounter = (decayDifference < B) ? (B - decayDifference - 1) : 0;
+          relativeEnvelopeCounter < samplesToRead && decayCounter > 0; relativeEnvelopeCounter++) {
+          decayCounter--;
+          buf[relativeEnvelopeCounter] *= (decayCounter/(float)decaySamples);
         }
       }
-  
-      /*for (releaseCounter = 0; releaseCounter < releaseSamples; releaseCounter++) {
-        buf[S-releaseCounter-1] = (float)buf[S-releaseCounter-1]*((float)releaseCounter/(float)releaseSamples);
-      }*/
   
       Audio.prepare(buf, samplesToRead, volume);
       Audio.write(buf, samplesToRead);
       
+      segmentCounter++;
       //adjust samples remaining
       samplesRemaining -= samplesToRead;
       if (samplesRemaining < samplesToRead) { samplesToRead = samplesRemaining; }
     }
+
+    grainWriteCounter++;
+    //seek back if we need to re-do this grain
+    if (grainWriteCounter < grainRepeat) {
+      wavFile.seek(grainPosition);
+    }
+    else {
+      grainWriteCounter = 0;
+      grainPosition = wavFile.position();
+    }
   }
-  myFile.close();
+  wavFile.close();
 }
